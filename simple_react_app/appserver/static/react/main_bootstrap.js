88234bec28a0b8569cc32c0fcbb1d32d42e2e851
Kyle Smith <ksmith@aplura.com>
2021-02-26 14:28:42 -0500

    Fri Feb 26 14:28:42 EST 2021 Completed React Native - no docs

--- simple_react_app/appserver/static/react/main_bootstrap.js
+++ simple_react_app/appserver/static/react/main_bootstrap.js
@@ -1,27 +1,21 @@
-require.config({
-    "paths": {
-        "main": "/static/app/simple_react_app/react/main.js?v=0cce19540a14ed830996",
-        "react": "/static/app/simple_react_app/react/react.js?v=0c283777d9c2f2dff7f1",
-        "react-dom": "/static/app/simple_react_app/react/react-dom.js?v=3e54d865f8abce15e263"
-    }, "shim": {"main": {"exports": ["main"]}, "react": {"exports": "react"}, "react-dom": {"exports": "react-dom"}}
-});
-require([
-    "splunkjs/ready!",
-    "splunkjs/mvc/simplexml/ready!",
-    "splunkjs/mvc/utils",
-    "main", "react", "react-dom", "backbone"
-], function (mvc,
-             ignored,
-             splunkjsUtils,
-             main, react, reactdom, backbone
-) {
-    reactdom.render(
-        react.createElement(main.default, {
-            splunkjs: mvc,
-            utils: splunkjsUtils,
-            splunk_components: {backbone: backbone,}
-        }),
-        document.getElementById("react-app")
-    );
-});
+require.config({"paths":{"main":"/static/app/simple_react_app/react/main.js?v=548f50cd75e4a51143dd","react":"/static/app/simple_react_app/react/react.js?v=0c283777d9c2f2dff7f1","react-dom":"/static/app/simple_react_app/react/react-dom.js?v=3e54d865f8abce15e263"},"shim":{"main":{"exports":["main"]},"react":{"exports":"react"},"react-dom":{"exports":"react-dom"}}});
+                require([
+                        "splunkjs/ready!",
+                        "splunkjs/mvc/simplexml/ready!",
+                        "splunkjs/mvc/utils",
+                        "main", "react", "react-dom", "backbone"
+                    ], function (mvc,
+                                 ignored,
+                                 splunkjsUtils,
+                                 main, react, reactdom, backbone
+                    ) {
+                        reactdom.render(
+                            react.createElement(main.default, {
+                                splunkjs: mvc,
+                                utils: splunkjsUtils,
+                                splunk_components: { backbone: backbone, }
+                                }),
+                            document.getElementById("react-app")
+                            );
+                    });
                 
--- simple_react_app/appserver/static/react_native/src/App.js
+++ simple_react_app/appserver/static/react_native/src/App.js
@@ -3,11 +3,19 @@ class App extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            "server_info": {}
+        }
         this.updateParentState = this.updateParentState.bind(this);
     }
 
     componentDidMount = () => {
         // do things on mount of component
+        // This will use the native splunk JS object to "get" an endpoint. "post" is also supported.
+        this.props.splunk.get("server/info").then((d)=>{
+            let clear = JSON.parse(d);
+            this.setState({"server_info": clear["entry"][0]["content"]})
+        })
     };
 
     updateParentState = (prop) => {
@@ -15,10 +23,15 @@ class App extends React.Component {
     };
 
     render() {
-        console.log(this.props.splunk);
         return (
             <div>
                 <h1>Simple React App</h1>
+                {Object.entries(this.state.server_info).map((i)=>{
+                    let k = i[0], v = i[1];
+                    if (["activeLicenseGroup", "cpu_arch", "host", "os_version", "os_name", "version"].includes(k)) {
+                        return (<div><strong>{k}</strong>: <i>{JSON.stringify(v) || "N/A"}</i></div>)
+                    }
+                })}
             </div>
         );
     }
